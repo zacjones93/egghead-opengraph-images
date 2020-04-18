91e23a029ca1956bbaa6d8d8b567a1ee6d2164fc
Daniel <Scriptor.Deo@gmail.com>
2020-04-18 10:54:04 -0400

    Upgraded cloudinary processing to use versions

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,23 +1,37 @@
-const qs = require('querystring')
+const cloudinary = require("cloudinary").v2;
+const qs = require('querystring');
+cloudinary.config({
+  cloud_name: "daxsro6st",
+  api_key: ProcessingInstruction.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 exports.handler = async function (event, ctx) {
   const {queryStringParameters} = event
   try {
-    const imageUrl = `https://res.cloudinary.com/${
-      process.env.CLOUD_NAME
-    }/image/fetch/${encodeURIComponent(
-      `https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
-        queryStringParameters,
-      )}`,
-    )}`
+    // https://res.cloudinary.com/daxsro6st/image/upload/v1587129280/og-images/1x1-0050ff7f_tocst6.png
+    const imageUrl = cloudinary.url(
+      `${process.env.DANIEL_IMAGE_VERSION}/og-images/1x1-0050ff7f_tocst6.png`,
+      {
+        // resouce_type: "raw"
+        sign_url: true,
+        // secure: true,
+        custom_pre_function: {
+          function_type: "remote",
+          source: `https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+            queryStringParameters
+          )}`
+        }
+      }
+    );
     return {
       statusCode: 302,
       headers: {
-        Location: imageUrl,
+        Location: imageUrl
       },
-      body: '',
-    }
+      body: ""
+    };
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
