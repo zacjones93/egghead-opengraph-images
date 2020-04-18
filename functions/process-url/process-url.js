f4bd441b0c4bce38bc4968b2bb57420b78e46865
Daniel <Scriptor.Deo@gmail.com>
2020-04-18 13:54:55 -0400

    changed back processurl changes

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -19,11 +19,10 @@ exports.handler = async function (event, ctx) {
         // secure: true,
         custom_pre_function: {
           function_type: "remote",
-          source: `${encodeURIComponent(
+          source: 
             `https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(queryStringParameters)
           }`
-          )
-        }`
+          
     }
       }
     );
