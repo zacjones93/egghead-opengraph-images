9f3f6d07084afa7077943f8456822854a93b822d
Daniel <Scriptor.Deo@gmail.com>
2020-04-18 13:40:58 -0400

    attempt to standardize query strings

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -19,10 +19,12 @@ exports.handler = async function (event, ctx) {
         // secure: true,
         custom_pre_function: {
           function_type: "remote",
-          source: `https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`
-        }
+          source: `${encodeURIComponent(
+            `https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(queryStringParameters)
+          }`
+          )
+        }`
+    }
       }
     );
     console.log(`https://quizzical-liskov-0bc7e1.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
