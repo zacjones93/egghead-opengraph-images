c453d19a4554c6a149cbf60ba437aab1076f4b2a
Daniel <Scriptor.Deo@gmail.com>
2020-04-18 11:59:07 -0400

    Added Description call in gen-opengraph-image

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -24,8 +24,9 @@ exports.handler = async function(event, ctx) {
   const { queryStringParameters } = event;
   await page.addScriptTag({
     content: `
-  window.timeToRead = "${queryStringParameters.author || ""}";
+  window.timeToRead = "${queryStringParameters.timeToRead || ""}";
   window.title = "${queryStringParameters.title || "No Title"}";
+  window.description = "${queryStringParameters.description || "No Description"}";
   window.author = "${queryStringParameters.author || ""}";
   `
   });
