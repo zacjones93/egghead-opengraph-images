2b2500d01c53cd260b4d1dd8303a7c6d1235a745
Daniel Chapman <Scriptor.Deo@gmail.com>
2020-05-11 23:02:01 -0400

    decode what was encoded.

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -24,9 +24,9 @@ exports.handler = async function(event, ctx) {
   const { queryStringParameters } = event;
   await page.addScriptTag({
     content: `
-  window.title = "${queryStringParameters.title || "No Title"}";
-  window.type = "${queryStringParameters.type || "" }";
-  window.author = "${queryStringParameters.author || ""}";
+  window.title = "${decodeURIComponent(queryStringParameters.title) || "No Title"}";
+  window.type = "${decodeURIComponent(queryStringParameters.type) || "No Type" }";
+  window.author = "${decodeURIComponent(queryStringParameters.author) || "No Author"}";
   `
   });
   
